767bd2efaa7b3d17c19e18561114cc42de9f85c7
Miguel Fuentes <miguel-fc@users.noreply.github.com>
2022-05-06 09:53:28 -0400

    Adding imports

--- LA.py
+++ LA.py
@@ -1,3 +1,16 @@
+import matplotlib.pyplot as plt # plotting library
+import numpy as np # this module is useful to work with numerical arrays
+import pandas as pd 
+import random 
+import torch
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader,random_split
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
 class LAutoencoder(nn.Module):
     
     def __init__(self,encoded_space_dim,fc2_input_dim):
