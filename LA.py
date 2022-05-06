7f9c8cd87c042b2dc4699a7d3387e3e8d9796408
Miguel Fuentes <miguel-fc@users.noreply.github.com>
2022-05-06 16:57:03 -0400

    Adding training and testing functiions

--- LA.py
+++ LA.py
@@ -39,3 +39,50 @@ class LAutoencoder(nn.Module):
             latent = self.encoder(x)
             x = self.decoder(latent)
             return x,latent
+
+    ### Training function
+    def train_epoch(model, device, dataloader, loss_fn, optimizer):
+        # Set train mode for both the encoder and the decoder
+        model.train()
+        # decoder.train()
+        train_loss = []
+        # Iterate the dataloader (we do not need the label values, this is unsupervised learning)
+        for data in dataloader: # with "_" we just ignore the labels (the second element of the dataloader tuple)
+            # Move tensor to the proper device
+            img, labels = data
+            img = img.view(img.size(0), -1).to(device)  
+            # Encode and Decode data
+            output,latent = model(img)
+            loss = loss_fn(output, img)
+            # Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            # Print batch loss
+            # print('\t train loss per batch: %f' % (loss.data))
+            train_loss.append(loss.detach().cpu().numpy())
+        return np.mean(train_loss)
+
+    s### Testing function
+    def test_epoch(model, device, dataloader, loss_fn):
+        # Set evaluation mode for encoder and decoder
+        model.eval()
+        # decoder.eval()
+        with torch.no_grad(): # No need to track the gradients
+            # Define the lists to store the outputs for each batch
+            conc_out = []
+            conc_label = []
+            for  data in dataloader:
+                img, labels = data
+                img = img.view(img.size(0), -1).to(device) 
+                # Encode and Decode data
+                output, latent = model(img)
+                # Append the network output and the original image to the lists
+                conc_out.append(output.cpu())
+                conc_label.append(img.cpu())
+            # Create a single tensor with all the values in the lists
+            conc_out = torch.cat(conc_out)
+            conc_label = torch.cat(conc_label) 
+            # Evaluate global loss
+            val_loss = loss_fn(conc_out, conc_label)
+        return val_loss.data
